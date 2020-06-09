e45ecd4a265f5a9266e40d81e9f06f0d6e6d1ec7
drybnikov <drybnikau@justaddmusic.net>
2020-06-09 16:13:47 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module my-elasticsearch-app
+
+go 1.14
+
+require (
+	github.com/elastic/go-elasticsearch/v8 v8.0.0-20200521065016-b05f73fe0dcf
+	github.com/gorilla/mux v1.7.4
+)
---
+++ main.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/gorilla/mux"
+)
+
+var es *elasticsearch.Client
+
+type errorResponse struct {
+	Error map[string]interface{}
+}
+
+func main() {
+	log.SetFlags(0)
+
+	var err error
+	es, err = elasticsearch.NewDefaultClient()
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
+
+	res, err := es.Info()
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+
+	defer res.Body.Close()
+	log.Println(res)
+
+	listen()
+}
+
+func listen() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/students", getStudents).Methods("GET")
+	router.HandleFunc("/students/{userID}", postStudents).Methods("POST")
+
+	router.HandleFunc("/store", getStore).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getStore(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Handle getStore")
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Query().Get("name"))
+}
+
+func postStudents(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
+
+	userID := -1
+	var err error
+	if val, ok := pathParams["userID"]; ok {
+		userID, err = strconv.Atoi(val)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"message": "need a number"}`))
+			return
+		}
+	}
+
+	query := r.URL.Query()
+	location := query.Get("location")
+
+	w.Write([]byte(fmt.Sprintf(`{"userID": %d, "location": "%s" }`, userID, location)))
+}
+
+func getStudents(w http.ResponseWriter, r *http.Request) {
+	var (
+		m map[string]interface{}
+	)
+	var buf bytes.Buffer
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"match": map[string]interface{}{
+				"name": r.URL.Query().Get("name"),
+			},
+		},
+	}
+
+	if err := json.NewEncoder(&buf).Encode(query); err != nil {
+		log.Fatalf("Error encoding query: %s", err)
+	}
+
+	log.Printf("Handle getSongs %s", query)
+
+	// Perform the search request.
+	response, err := es.Search(
+		es.Search.WithContext(context.Background()),
+		es.Search.WithIndex("students"),
+		es.Search.WithBody(&buf),
+		es.Search.WithTrackTotalHits(true),
+		es.Search.WithPretty(),
+	)
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+	defer response.Body.Close()
+	log.Printf("%s", response)
+
+	if response.IsError() {
+		var e errorResponse
+		if err := json.NewDecoder(response.Body).Decode(&e); err != nil {
+			log.Fatalf("Error parsing the response body: %s", err)
+		} else {
+			// Print the response status and error information.
+			log.Fatalf("[%s] %s: %s",
+				response.Status(),
+				e.Error["type"],
+				e.Error["reason"],
+			)
+		}
+	}
+	if err := json.NewDecoder(response.Body).Decode(&m); err != nil {
+		log.Fatalf("Error parsing the response body: %s", err)
+	}
+	var students []Student
+	for _, hit := range m["hits"].(map[string]interface{})["hits"].([]interface{}) {
+		// Parse the attributes/fields of the document
+		doc := hit.(map[string]interface{})["_source"].(map[string]interface{})
+
+		log.Printf("doc %s", doc)
+
+		newStudent := Student{
+			Name:         doc["name"].(string),
+			Age:          int64(doc["age"].(float64)),
+			AverageScore: doc["average_score"].(float64),
+		}
+
+		students = append(students, newStudent)
+	}
+
+	if err := json.NewEncoder(&buf).Encode(students); err != nil {
+		log.Fatalf("Error encoding students: %s", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, buf.String())
+}
+
+type Student struct {
+	Name         string  `json:"name"`
+	Age          int64   `json:"age"`
+	AverageScore float64 `json:"average_score"`
+}
